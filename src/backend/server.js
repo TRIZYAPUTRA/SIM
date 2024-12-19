c74b88e7597e28061843e0f25d97bee36b11dd22
TRIZYAPUTRA <149507782+TRIZYAPUTRA@users.noreply.github.com>
2024-12-20 05:28:20 +0700

    Update server.js

--- src/backend/server.js
+++ src/backend/server.js
@@ -21,8 +21,7 @@ const db = mysql.createConnection({
   password: "AVNS_NlALO0ONDh4mfDAqt9b",
   database: "sim_app",
   port: "3157",
-  ssl: { rejectUnauthorized: false },
-  connectTimeout: 10000,  // Menonaktifkan verifikasi sertifikat
+   // Menonaktifkan verifikasi sertifikat
   },
 });
 
